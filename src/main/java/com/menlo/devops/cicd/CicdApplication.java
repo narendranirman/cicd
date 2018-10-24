552ae0b8f27b8677474ce1d7dbfa10f0115234b7
narendrad <narendrad@menlo-technologies.com>
2018-10-24 12:04:39 +0530

    --initial commit

---
+++ src/main/java/com/menlo/devops/cicd/CicdApplication.java
@@ -0,0 +1,12 @@
+package com.menlo.devops.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+}
